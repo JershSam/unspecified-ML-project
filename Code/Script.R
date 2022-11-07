934cef3a599d33d28056f247b3cfe1fbf6574b56
Brian Somoukian-Mostlag <joshsomoukian@datadisciples.com>
2022-11-07 13:36:53 -0500

    Initial work and setup on the ML project. Built out some notes and imported the data while also making sure the columns are read in properly.

---
+++ Code/Script.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(ggplot2)
+
+assignments = read_csv("ML Project/assignments.csv", 
+                        col_types = cols(assignment_created_at = col_datetime(format = "%Y-%m-%d %H:%M:%S"), 
+                                         start_date = col_datetime(format = "%Y-%m-%d %H:%M:%S"), 
+                                         end_date = col_datetime(format = "%Y-%m-%d %H:%M:%S"), 
+                                         filled_date = col_datetime(format = "%Y-%m-%d %H:%M:%S"), 
+                                         hours_worked = col_double(),
+                                         bonus_percent = col_double(),
+                                         admin_bonus_percent = col_double(),
+                                         bonus_added_at = col_datetime(format = "%Y-%m-%d %H:%M:%S"),
+                                         professional_removed_at = col_datetime(format = "%Y-%m-%d %H:%M:%S")))
+
+contractor_availability_true = read_csv("ML Project/contractor-availability-true.csv", 
+                                        col_types = cols(date = col_date(format = "%Y-%m-%d")))
+
+contractor_geolocations_distance = read.csv('~/R/unspecified-ML-project/ML Project/contractor-geolocations-distance.csv')
+
+office_alternative_locations = read.csv('~/R/unspecified-ML-project/ML Project/office-alternative-locations.csv')
+
+office_geolocations = read.csv('~/R/unspecified-ML-project/ML Project/office-geolocations.csv')
+
+office_softwares = read.csv('~/R/unspecified-ML-project/ML Project/office-softwares.csv')
+
+
+#Create relevant columns for assignments
+assignments$is_filled = if_else(assignments$contractor_id == 'NULL', 0, 1)
+
+#Create assignments df that's only past assignments
+past_assignments = assignments %>%
+  filter(end_date < Sys.Date())
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # unspecified-ML-project
+
+Tasks:
+
+- Determine how to generate a "Supply" for any given work site (NEED TO COMPLETE THE HIGH COMPLEXITY TASK AT THE BOTTOM OF THIS LIST FIRST)
+- Likewise, generate a "demand"
+- Create an "is_urgent" column
+- Create a "shift_length" column
+- Create a "day_of_week" column (use numbered days)
+- Create a season column
+
+
+- Slightly more complex task: they want insights based on the software used by the clinic. Let's create a numbered key for all of the different softwares used, and then join that key to the assignments dataset rather than join the names of the softwares. That way we can more easily feed the data to a ML algorithm
+
+- High Complexity: Seems as though determining what constitutes an "available contractor" includes calculating their distance in miles from a location. Going to have to do this through latitudes and longitudes.
+
+Questions to Ask:
+
+- There are two position_id's (1 and 2). position_id indicates the type of contractor, but there isn't any info about what those types are.
+- In demand it says "# of other shifts available in that area". What is an 'area' in this context? Is it an office_id? Or is it a radius around that specific office_id?
+- Similarly, "Location of clients work site". What exactly does location refer to here? Is it just the latitude and longitude? Or are we trying to analyze the city/zip/county/state?
+- For urgency, what constitutes a last minute shift? How close does the creation of the assignment need to be to the start of the shift for us to flag it down as urgent
+- Ask if they want us to calculate distance as just a straight line from Point A to Point B, or if they want the distance to be a driving distance instead
