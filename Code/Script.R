799a48d1de0eb8323ddb0cc0212985975a27236d
Brian Somoukian-Mostlag <joshsomoukian@datadisciples.com>
2022-11-08 15:05:53 -0500

    Created markdown file that currently is outputting an in depth summary statistics of all the relevant variables in our data

--- Code/Script.R
+++ Code/Script.R
@@ -12,6 +12,11 @@ assignments = read_csv("ML Project/assignments.csv",
                                          bonus_added_at = col_datetime(format = "%Y-%m-%d %H:%M:%S"),
                                          professional_removed_at = col_datetime(format = "%Y-%m-%d %H:%M:%S")))
 
+
+
+
+
+
 contractor_availability_true = read_csv("ML Project/contractor-availability-true.csv", 
                                         col_types = cols(date = col_date(format = "%Y-%m-%d")))
 
@@ -24,7 +29,7 @@ office_geolocations = read.csv('~/R/unspecified-ML-project/ML Project/office-geo
 office_softwares = read.csv('~/R/unspecified-ML-project/ML Project/office-softwares.csv')
 
 
-#Create relevant columns for assignments
+#Create relevant columns for assignmentsassignments
 assignments$is_filled = if_else(assignments$contractor_id == 'NULL', 0, 1)
 
 #Create assignments df that's only past assignments
